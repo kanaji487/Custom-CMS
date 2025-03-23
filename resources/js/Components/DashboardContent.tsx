4d6592e0c48cbd54b2e941eaa835d5696ee192f9
Auttawut <kzcofx@gmail.com>
2025-03-23 19:48:33 +0700

    Create sidebar by using shadcn/ui.

---
+++ resources/js/Components/DashboardContent.tsx
@@ -0,0 +1,13 @@
+export default function DashboardContent(){
+    return(
+        <div className="py-12">
+            <div className="mx-auto max-w-full sm:px-6 lg:px-8">
+                <div className="overflow-hidden bg-black shadow-sm sm:rounded-lg dark:bg-gray-800">
+                    <div className="p-6 text-white dark:text-gray-100">
+                        You're logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
